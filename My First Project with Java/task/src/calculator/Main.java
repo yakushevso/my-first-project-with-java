d824bec0a12522ef0d00eab439c8aec74e82d063
Work <yakushevso@ya.ru>
2024-03-18 11:34:56 +0300

    Stage 3/3: Calculate net income

--- My First Project with Java/task/src/calculator/Main.java
+++ My First Project with Java/task/src/calculator/Main.java
@@ -1,29 +1,43 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        float bubblegum = 202;
-        float toffee = 118;
-        float iceCream = 2250;
-        float milkChocolate = 1680;
-        float doughnut = 1075;
-        float pancake = 80;
+        try (Scanner sc = new Scanner(System.in)) {
 
-        float income = bubblegum + toffee + iceCream
-                + milkChocolate + doughnut + pancake;
+            float bubblegum = 202;
+            float toffee = 118;
+            float iceCream = 2250;
+            float milkChocolate = 1680;
+            float doughnut = 1075;
+            float pancake = 80;
 
-        System.out.printf("""
-                Earned amount:
-                Bubblegum: $%.1f
-                Toffee: $%.1f
-                Ice cream: $%.1f
-                Milk chocolate: $%.1f
-                Doughnut: $%.1f
-                Pancake: $%.1f
-                """,
-                bubblegum, toffee, iceCream,
-                milkChocolate, doughnut, pancake);
+            float income = bubblegum + toffee + iceCream
+                    + milkChocolate + doughnut + pancake;
 
-        System.out.printf("Income: $%.1f", income);
+            System.out.printf("""
+                            Earned amount:
+                            Bubblegum: $%.1f
+                            Toffee: $%.1f
+                            Ice cream: $%.1f
+                            Milk chocolate: $%.1f
+                            Doughnut: $%.1f
+                            Pancake: $%.1f
+                            """,
+                    bubblegum, toffee, iceCream,
+                    milkChocolate, doughnut, pancake);
+
+            System.out.printf("Income: $%.1f\n", income);
+
+            System.out.println("Staff expenses:");
+            float staffExpenses = sc.nextInt();
+            System.out.println("Other expenses:");
+            float otherExpenses = sc.nextInt();
+
+            float netIncome = income - (staffExpenses + otherExpenses);
+
+            System.out.printf("Net income: $%.1f", netIncome);
+        }
     }
 }
--- My First Project with Java/task/test/MyFirstProjectTest.java
+++ My First Project with Java/task/test/MyFirstProjectTest.java
@@ -15,6 +15,9 @@ import java.util.Map;
 // Pancake: $80.0
 //
 // Income: $5405.0
+// Staff expenses: $4170
+// Other expenses: $220
+// Net income: $1015
 
 public class MyFirstProjectTest extends StageTest {
     @DynamicTest
@@ -22,12 +25,11 @@ public class MyFirstProjectTest extends StageTest {
         TestedProgram pr = new TestedProgram();
 
         String output = pr.start().toLowerCase().strip();
-        int output_length = (int) Arrays.stream(output.split("\n")).filter(line -> !line.isEmpty()).count();
         if (output.isEmpty()) {
             return CheckResult.wrong("Your program didn't print any output.");
-        } else if (output_length != 8) {
-            return CheckResult.wrong(String.format("Your program should output 8 lines, but %s lines were found.",
-                    output_length));
+        } else if (output.contains(">")) {
+            return CheckResult.wrong("The greater-than symbol followed by a space (> ) represents the user input " +
+                    "in examples. It's not part of the input and you shouldn’t print this sign");
         }
         if (!output.contains("earned")) {
             return CheckResult.wrong("Your program didn't print the 'Earned amount' line");
@@ -44,18 +46,41 @@ public class MyFirstProjectTest extends StageTest {
             if (!output.contains(name.toLowerCase())) {
                 return CheckResult.wrong(String.format("Your program should print the '%s' as an item", name));
             }
-        }
-        if (!output.contains("income")) {
-            return CheckResult.wrong("Your program should print the 'Income' on a separate line");
-        }
-        for (String name : map.keySet()) {
             if (!output.contains(map.get(name))) {
                 return CheckResult.wrong(String.format("Incorrect earned amount for %s.", name));
             }
         }
+        if (!output.contains("income")) {
+            return CheckResult.wrong("Your program should print the 'Income' on a separate line");
+        }
         if (!output.contains("5405")) {
             return CheckResult.wrong("Incorrect total income! Make sure you didn’t print the total income value from " +
-                    "the examples, calculate it based on the earnings from the table in description.");
+                    "the examples, calculate it based on the earnings mentioned in previous stage.");
+        }
+        if (!output.contains("staff expenses")) {
+            return CheckResult.wrong("Your program should ask the user for input with the 'Staff expenses' line");
+        }
+        if (output.contains("other expenses") || output.contains("net income")) {
+            return CheckResult.wrong("When the program has just started, you should not yet print the 'Other " +
+                    "expenses' or 'Net income' lines, as no input has been provided yet.");
+        }
+        if (!pr.isWaitingInput()) {
+            return CheckResult.wrong("Your program should ask the user to input the staff expenses");
+        }
+        String output1 = pr.execute("4170").toLowerCase().strip();
+        if (!output1.contains("other expenses")) {
+            return CheckResult.wrong("Your program should ask the user for input with the 'Other expenses' line");
+        }
+        if (!pr.isWaitingInput()) {
+            return CheckResult.wrong("Your program should ask the user to input other expenses");
+        }
+        String output2 = pr.execute("220").toLowerCase().strip();
+        if (!output2.contains("net income")) {
+            return CheckResult.wrong("Your program should print the 'Net income' on a separate line");
+        } else if (!output2.contains("1015")) {
+            return CheckResult.wrong(
+                    "Incorrect net income! Make sure you didn’t print the net income value from the examples" +
+                            ", calculate it by subtracting expenses provided in the input from the total income");
         }
         return CheckResult.correct();
     }
